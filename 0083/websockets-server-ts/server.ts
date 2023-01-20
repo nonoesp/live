bf191cbe5cb7544c620c4569af086d619d092be3
Nono Martínez Alonso <mundowarezweb@gmail.com>
2023-01-20 11:57:53 +0100

    83 · Types and on connection message.

--- 0083/websockets-server-ts/server.ts
+++ 0083/websockets-server-ts/server.ts
@@ -5,10 +5,12 @@ import { WebSocketServer } from "ws";
 const port = 1234;
 const wss = new WebSocketServer({port});
 
-wss.on('connection', (ws) => {
+wss.on('connection', (ws: any) => {
     // Handles new connections
 
-    ws.on('message', (data) => {
+    console.log(`New client connected.`);
+
+    ws.on('message', (data: any) => {
         console.log(`Received message from client: ${data}`);
     })
 
